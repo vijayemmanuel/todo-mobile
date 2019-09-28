049e8ebedc86152c0a4885d2aed5bbd379f0667b
VIJAY PAZHEPARAMPIL <vijay.emmanuel@gmail.com>
2019-09-28 23:09:42 +0530

    Added Vector Icons

--- README.md
+++ README.md
@@ -27,6 +27,11 @@ Building for Android
 
 >sbt android:prod
 
+Add Vector icons
+Edit android/app/build.gradle ( NOT android/build.gradle ) and add the following:
+apply from: "../../node_modules/react-native-vector-icons/fonts.gradle"
+>eact-native link react-native-vector-icons
+
 Running Android Virtual Device
 >react-native run-android
 
--- src/main/scala/sri/mobile/todo/components/package.scala
+++ src/main/scala/sri/mobile/todo/components/package.scala
@@ -9,29 +9,29 @@ package object components {
   val root = TabNavigator(
     TabNavigatorConfig(
       tabBarOptions = TabBarOptions(
-        activeTintColor = if (SriPlatform.isIOS) "#e91e63" else "#fff")
+          activeTintColor = if (SriPlatform.isIOS) "#e91e63" else "#fff", showIcon = true)
+
     ),
     registerTabScreen[BuyScreen](
         navigationOptions = NavigationTabScreenOptions(
         tabBarIcon = (iconOptions: IconOptions) => {
           Ionicons(
             name =
-              if (SriPlatform.isAndroid) IoniconsList.IOS_HOME
-              else IoniconsList.IOS_HOME_OUTLINE,
+              if (SriPlatform.isAndroid) IoniconsList.IOS_ADD_CIRCLE
+              else IoniconsList.IOS_ADD_CIRCLE_OUTLINE,
             size = 27,
             style = GlobalStyles.dynamicColor(iconOptions.tintColor)
           )
         },
-        tabBarLabel = "Buy",
-
+        tabBarLabel = "Buy"
       )),
     registerTabScreen[BoughtScreen](
       navigationOptions = NavigationTabScreenOptions(
         tabBarIcon = (iconOptions: IconOptions) => {
           Ionicons(
             name =
-              if (iconOptions.focused) IoniconsList.IOS_SETTINGS
-              else IoniconsList.IOS_SETTINGS_OUTLINE,
+              if (iconOptions.focused) IoniconsList.IOS_REMOVE_CIRCLE
+              else IoniconsList.IOS_REMOVE_CIRCLE_OUTLINE,
             size = 26,
             style = GlobalStyles.dynamicColor(iconOptions.tintColor)
           )
